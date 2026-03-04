1a9c5e90194ec800c6ce8f9d54c5a34358ddb213
suku156 <lb48673321@gmail.com>
2026-03-04 16:19:00 +0800

    拆解了主流程,複製程序過去還未將其從原本的地方移除的版本

--- src/MainProcess_Module/RTFProcessor.h
+++ src/MainProcess_Module/RTFProcessor.h
@@ -1,1 +1,16 @@
 ﻿#pragma once
+#include<filesystem>
+#include<optional>
+
+// 用來區分處理的是 單獨檔案 還是 資料夾
+enum class ProcessMode{SingleFile,BatchFile};
+
+// 主流程
+class RTFProcessor{
+public:
+  void processFile(const std::filesystem::path& filePath,
+                    ProcessMode mode,
+                    std::optional<std::filesystem::path> taskRootDir = std::nullopt);
+};
+
+
