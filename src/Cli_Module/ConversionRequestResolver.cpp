c0845eb2e60bc6d9078217cf552329cee6db0539
suku156 <lb48673321@gmail.com>
2026-03-10 12:01:46 +0800

    初步制定了中轉層的概念

---
+++ src/Cli_Module/ConversionRequestResolver.cpp
@@ -0,0 +1,48 @@
+// =====================================================
+// Module : ConversionRequestResolver (implementation)
+// =====================================================
+#include "ConversionRequestResolver.h"
+#include "Universal_Module/CommonEnum.h"
+#include "CliParser.h"
+#include <filesystem>
+#include <system_error>
+#include <string>
+#include <cwctype>
+
+Conversion::ResolvedConfig Conversion::resolveConfig(const Cli::ParseResult& parConfig){
+  Conversion::ResolvedConfig resultconfig;
+
+  // 先把相對路徑轉成絕對路徑
+  std::filesystem::path input  = std::filesystem::absolute(parConfig.config.inputPath);
+  std::filesystem::path output;
+  std::error_code ec;
+  if(parConfig.config.outputDir.has_value()){
+    
+    output = std::filesystem::absolute(
+             parConfig.config.outputDir.value());
+
+  }
+  else{
+    
+    if (std::filesystem::is_regular_file(input, ec)){
+      output = input.parent_path(); 
+    }
+    else if(std::filesystem::is_directory(input, ec)){
+      output = input;  
+    }
+    else{
+      output = input.parent_path();  
+    }
+  }
+
+  resultconfig.inputPath = input;
+  resultconfig.outputDir = output;
+  // 處理 format
+  if(parConfig.config.format.has_value()){
+    resultconfig.format = parConfig.config.format.value();
+  }else{
+    resultconfig.format = Cli::OutputFormat::Txt;
+  }
+
+  return resultconfig;
+}
---
+++ src/Cli_Module/ConversionRequestResolver.h
@@ -0,0 +1,36 @@
+// =====================================================
+// Module  : ConversionRequestResolver
+// Author  : suku156
+// Purpose : 負責轉換固定資料給執行層
+// Layer   : Cli
+//
+// Depend  :
+//   CliParser
+//
+// Used by :
+//   ConversionEngine   
+//
+// Notes :
+//   將 CliParser 解析得到的資料,確定成固定形式給 ConversionEngine 使用
+// =====================================================
+#pragma once
+#include <filesystem>
+#include <string>
+#include "Universal_Module/CommonEnum.h"
+
+namespace Cli{
+   struct ParseResult;
+   enum class OutputFormat;
+};
+
+namespace Conversion {
+  struct ResolvedConfig{
+    std::filesystem::path inputPath;
+    std::filesystem::path outputDir;
+    Cli::OutputFormat format;
+    std::wstring basename;
+  };
+
+  ResolvedConfig resolveConfig(const Cli::ParseResult& parConfig);
+};
+
