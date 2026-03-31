fb2657ea4755ead5c2cdfb2ee9146462837465bb
suku156 <lb48673321@gmail.com>
2026-03-31 17:35:56 +0800

    在ｇｕｉ　中加入了顯示ｌｏｇ區來擴充回饋＂

--- Qt_gui_Module/mainwindow.cpp
+++ Qt_gui_Module/mainwindow.cpp
@@ -46,6 +46,10 @@ MainWindow::MainWindow(QWidget *parent)
     ui->lineEdit_threadNum->setValidator(new QIntValidator(1,16,this));
     ui->lineEdit_threadNum->setPlaceholderText("預設自動(可以不填)");
     ui->label_status->setText("準備中");
+
+    // 詳細資訊欄顯示
+    ui->plainTextEdit_log->setReadOnly(true);
+    ui->plainTextEdit_log->setLineWrapMode(QPlainTextEdit::NoWrap);
 }
 
 GuiFormData MainWindow::collectFormData() const{
@@ -69,41 +73,67 @@ void MainWindow::on_btnConvert_clicked(){
 
     ConvertUiGuard guard(ui->btnConvert,ui->label_status);
 
+    appendLog("=====轉換開始=====");
+
     GuiFormData form = collectFormData();
+    appendLog("載入 GUI 請求...");
     GuiRequestTranslator request;
     auto result = request.translate(form);
+    appendLog("開始轉譯 GUI 請求...");
     if(!result.ok){
+        appendLog("轉譯失敗: " + QString::fromStdWString(result.message));
         QMessageBox::warning(
             this,
             QStringLiteral("錯誤"),
             QString::fromStdWString(result.message));
         return;
     }
+    appendLog("轉譯成功");
 
     auto& req = result.Normalizedrequest;
 
     // 呼叫並連結核心功能
     App::ConversionEngine conversionengine;
     taskBuilder::ConversionTaskBuilder builder;
+    appendLog("開始建立轉換任務...");
     BuildResult BDresult = builder.build(req);
+    appendLog("轉換任務建立成功");
+    appendLogToBuildResult(BDresult);
+    appendLog("開始核心轉換流程");
     App::AppExitCode resultCode = conversionengine.run(BDresult);
 
     // 依據回傳結果做出回饋
     switch (resultCode) {
     case App::AppExitCode::Success :
-        QMessageBox::information(this, "完成", "轉換成功");
-        break;
+        {
+            appendLog("轉換成功");
+            QString pathresult = QString::fromStdWString(req.inputPath.filename().wstring());
+            QString successresult = "轉換成功,已處理目標: " + pathresult ;
+            QMessageBox::information(this, "完成",successresult);
+            break;
+        }
     case App::AppExitCode::Fail :
     case App::AppExitCode::RunTimeError:
-        QMessageBox::critical(this, "轉換失敗", "轉換過程發生錯誤");
-        break;
+        {
+            appendLog("轉換失敗");
+            QMessageBox::critical(this, "轉換失敗", "轉換過程發生錯誤");
+            break;
+        }
     case App::AppExitCode::PartialSuccess:
-        QMessageBox::warning(this, "部分完成", "部分檔案轉換成功，部分失敗");
-        break;
+        {
+            appendLog("轉換部份成功");
+            QMessageBox::warning(this, "部分完成", "部分檔案轉換成功，部分失敗");
+            break;
+        }
     defult:
-        QMessageBox::warning(this, "未知狀態", "發生未知錯誤");
-        break;
+        {
+            appendLog("未知錯誤");
+            QMessageBox::warning(this, "未知狀態", "發生未知錯誤");
+            break;
+        }
     }
+    appendLog("=====轉換結束=====");
+    ui->plainTextEdit_log->appendPlainText("");
 }
 
 void MainWindow::on_btnSelectInputFile_clicked(){
@@ -120,6 +150,7 @@ void MainWindow::on_btnSelectInputFile_clicked(){
     ui->InputInfo->setText(file);
     ui->InputInfo->adjustSize();
 
+    inputTargetType_ = InputTargetType::File;
 }
 
 void MainWindow::on_btnSelectInputDir_clicked(){
@@ -135,6 +166,7 @@ void MainWindow::on_btnSelectInputDir_clicked(){
 
     ui->InputInfo->setText(dir);
     ui->InputInfo->adjustSize();
+    inputTargetType_ = InputTargetType::Directory;
 }
 
 void MainWindow::on_btnSelectOutput_clicked(){
@@ -209,3 +241,61 @@ void MainWindow::debugPrintRequest(const NormalizedConversionRequest& req){
         qDebug() << "preserveRelativeStructure = false";
     }
 }
+
+void MainWindow::appendLog(const QString& text){
+    QString line = QTime::currentTime().toString("[HH:mm:ss] ") + text;
+    ui->plainTextEdit_log->appendPlainText(line);
+}
+
+void MainWindow::on_btnCleanLog_clicked(){
+    ui->plainTextEdit_log->clear();
+}
+
+void MainWindow::appendLogToBuildResult(const BuildResult& buildresult){
+    if(inputTargetType_ == InputTargetType::File){
+        appendLog("輸入模式 : 單檔");
+    }
+    else if(inputTargetType_ == InputTargetType::Directory){
+        appendLog("輸入模式 : 資料夾");
+    }
+    appendLog("輸入路徑 : " + QString::fromStdWString(buildresult.task.inputPath.wstring()));
+    appendLog("轉譯結果(非最終) 輸出目錄 : " + QString::fromStdWString(buildresult.task.outputDir.wstring()));
+
+    switch(buildresult.task.format){
+      case Common::OutputFormat::Txt :
+        appendLog("輸出格式 : txt");
+        break;
+      case Common::OutputFormat::Md  :
+        appendLog("輸出格式 : md");
+        break;
+      case Common::OutputFormat::Html:
+        appendLog("輸出格式 : html");
+        break;
+    }
+
+    if(inputTargetType_ == InputTargetType::Directory){
+        switch(buildresult.task.dirPolicy){
+        case Common::ExistingDirPolicy::Reject :
+            appendLog("輸出資料夾同名處裡策略為: 安全模式");
+            break;
+        case Common::ExistingDirPolicy::Overwrite :
+            appendLog("輸出資料夾同名處裡策略為: 覆蓋模式");
+            break;
+        case Common::ExistingDirPolicy::RenameWithSuffix :
+            appendLog("輸出資料夾同名處裡策略為: 重新命名模式");
+            break;
+        }
+
+        if(buildresult.task.recursive){
+            appendLog("遞迴模式 : 開啟");
+            if(buildresult.task.preserveRelativeStructure){
+                appendLog("保留中間資料夾路徑 : 保留");
+            }else{
+                appendLog("保留中間資料夾路徑 : 不保留");
+            }
+        }else{
+            appendLog("遞迴模式 : 關閉");
+        }
+    }
+}
+
--- Qt_gui_Module/mainwindow.h
+++ Qt_gui_Module/mainwindow.h
@@ -5,6 +5,12 @@
 #include "GuiFormData.h"
 
 struct NormalizedConversionRequest;
+struct BuildResult;
+enum class InputTargetType{
+   None,
+   File,
+   Directory
+};
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -28,15 +34,19 @@ private slots:
     void on_btnSelectOutput_clicked();
     void on_btnConvert_clicked();
     void on_btnCleanOutput_clicked();
+    void on_btnCleanLog_clicked();
 
 private:
     Ui::MainWindow *ui;
     QString selectedInputPath_;
     QString selectedOutputPath_;
+    InputTargetType inputTargetType_ = InputTargetType::None;
 
 private:
     void debugPrintRequest(const NormalizedConversionRequest& req);
     void updateOutputDisplay();
+    void appendLog(const QString& text);
+    void appendLogToBuildResult(const BuildResult& buildresult);
 
 };
 #endif // MAINWINDOW_H
